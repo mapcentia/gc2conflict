29042837807089a35c387004f5823413b8263d8c
Martin Høgh <mh@mapcentia.com>
2015-04-24 14:33:37 +0200

    Grunt file bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,7 +46,7 @@ module.exports = function (grunt) {
         gitreset: {
             production: {
                 options: {
-                    hard: true
+                    mode: 'hard'
                 }
             }
         }
