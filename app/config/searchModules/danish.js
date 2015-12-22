346f6f70ca9bac38687e3b6ef27c78af651acca1
Martin Høgh <mh@mapcentia.com>
2015-12-22 21:21:00 +0100

    AWS4

--- app/config/searchModules/danish.js
+++ app/config/searchModules/danish.js
@@ -37,7 +37,7 @@ createSearch = function (me) {
             (function ca() {
                 $.ajax({
                     url: 'http://eu1.mapcentia.com/api/v1/elasticsearch/search/dk/aws4/' + type1,
-                    data: '&q={"query":{"filtered":{"query":{"query_string":{"default_field":"string","query":"' + encodeURIComponent(query.toLowerCase().replace(",", "")) + '","default_operator":"AND"}},"filter":{"term":{"municipalitycode":"0' + komKode + '"}}}}}',
+                    data: '&q={"query":{"filtered":{"query":{"query_string":{"default_field":"string","query":"' + encodeURIComponent(query.toLowerCase().replace(",", "")) + '","default_operator":"AND"}},"filter":{"term":{"kommunekode":"0' + komKode + '"}}}}}',
                     contentType: "application/json; charset=utf-8",
                     scriptCharset: "utf-8",
                     dataType: 'jsonp',
